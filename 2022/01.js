c6119598623a683640c9c6ae42248aacb7067650
hlischt <50388907+hlischt@users.noreply.github.com>
2022-12-03 01:37:45 -0600

    Fix ugly spacing with the power of automatic formatting
    Using the following command:
    deno fmt --ext js
      --options-single-quote
      --options-line-width 100
      --options-use-tabs
    
    The two-tab thing comes from my emacs setup, configured to use tabs
    almost everywhere, and from js2-mode's apparent confusion between
    spaces and tabs. I've gotta fix that tomorrow.

--- 2022/01.js
+++ 2022/01.js
@@ -1,8 +1,13 @@
 const input = document.querySelector('pre').textContent;
-const common = (item) => item.split('\n\n')
-			.map(item => item.trim().split('\n')
-					 .reduce((acc, item) => Number(item) + acc, 0))
-			.sort((a, b) => b-a);
-const sol1 = item => common(input)[0];
-const sol2 = item => common(input).slice(0,3).reduce((acc, item) => acc+item, 0);
+const common = (item) =>
+	item.split('\n\n')
+		.map((item) =>
+			item.trim().split('\n')
+				.reduce((acc, item) => Number(item) + acc, 0)
+		)
+		.sort((a, b) => b - a);
+const sol1 = (item) => common(input)[0];
+const sol2 = (item) =>
+	common(input).slice(0, 3)
+		.reduce((acc, item) => acc + item, 0);
 console.log(`Part 1: ${sol1(input)}\nPart 2: ${sol2(input)}`);
--- 2022/02.js
+++ 2022/02.js
@@ -1,58 +1,61 @@
 const logic = {
-		// winner: 'loser'
-		rock: 'scissors',
-		paper: 'rock',
-		scissors: 'paper'
+	// winner: 'loser'
+	rock: 'scissors',
+	paper: 'rock',
+	scissors: 'paper',
 };
 const meaning = {
-		A: 'rock', X: 'rock',
-		B: 'paper', Y: 'paper',
-		C: 'scissors', Z: 'scissors',
+	A: 'rock',
+	X: 'rock',
+	B: 'paper',
+	Y: 'paper',
+	C: 'scissors',
+	Z: 'scissors',
 };
 const options = ['rock', 'paper', 'scissors'];
 
 options.loserAgainst = function (num) {
-		return mod((num - 1), this.length);
+	return mod(num - 1, this.length);
 };
 
 options.winnerAgainst = function (num) {
-		return (num + 1) % this.length;
+	return (num + 1) % this.length;
 };
 
 function mod(num, limit) {
-		return ((num % limit) + limit) % limit;
+	return ((num % limit) + limit) % limit;
 }
 
 function rps(opponent, you) {
-		const points = you.codePointAt() - 87; // 'X' is ASCII 88
-		if (meaning[opponent] === meaning[you]) { // draw
-				return points + 3;
-		}
-		if (logic[meaning[opponent]] === meaning[you]) { // opponent wins
-				return points;
-		}
-		return points + 6; // you win
+	const points = you.codePointAt() - 87; // 'X' is ASCII 88
+	if (meaning[opponent] === meaning[you]) { // draw
+		return points + 3;
+	}
+	if (logic[meaning[opponent]] === meaning[you]) { // opponent wins
+		return points;
+	}
+	return points + 6; // you win
 }
 
 function parseInput(input) {
-		return input.trim().split('\n').map(x => x.split(' '));
+	return input.trim().split('\n').map((x) => x.split(' '));
 }
 
 function part1(input) {
-		return parseInput(input).map(x => rps(...x))
-				.reduce((acc, item) => acc+item, 0);
+	return parseInput(input).map((x) => rps(...x))
+		.reduce((acc, item) => acc + item, 0);
 }
 
 function part2(input) {
-		const conditions = {
-				X: (x => options.loserAgainst(options.indexOf(x)) + 1),
-				Y: (x => options.indexOf(x) + 4),
-				Z: (x => options.winnerAgainst(options.indexOf(x)) + 7)
-		};
-		let count = 0;
-		for (let i of parseInput(input)) {
-				const func = conditions[i[1]];
-				count += func(meaning[i[0]]);
-		}
-		return count;
+	const conditions = {
+		X: ((x) => options.loserAgainst(options.indexOf(x)) + 1),
+		Y: ((x) => options.indexOf(x) + 4),
+		Z: ((x) => options.winnerAgainst(options.indexOf(x)) + 7),
+	};
+	let count = 0;
+	for (let i of parseInput(input)) {
+		const func = conditions[i[1]];
+		count += func(meaning[i[0]]);
+	}
+	return count;
 }
