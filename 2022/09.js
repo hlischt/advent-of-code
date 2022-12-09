625aaf27cef1ed89875f5ea7d21b54c7577e555e
hlischt <50388907+hlischt@users.noreply.github.com>
2022-12-09 02:09:30 -0600

    Move methods to prototypes
    This way, methods are shared among instances of a
    "class"/constructor/object instead of being instanciated for each
    call, and that would be wasteful.
    
    Also, throw an error when trying to create a Rope with less than 1 element.

--- 2022/09.js
+++ 2022/09.js
@@ -3,65 +3,68 @@
 function Point(x, y) {
 	this.x = x;
 	this.y = y;
-
-	this.neighbors = function (point) {
-		return (Math.abs(this.x - point.x) <= 1 &&
-			Math.abs(this.y - point.y) <= 1);
-	};
-
-	this.move = function (direction, following = null) {
-		// Moves only 1 cell
-		const dirs = {
-			U: [0, 1],
-			D: [0, -1],
-			R: [1, 0],
-			L: [-1, 0],
-		};
-		const [x, y] = dirs[direction];
-		if (following) {
-			this.moveFollowing(following);
-		} else {
-			this.x += x;
-			this.y += y;
-		}
-	};
-
-	this.moveFollowing = function (following) {
-		if (!this.neighbors(following)) {
-			if (following.x === this.x) {
-				following.y > this.y ? this.y++ : this.y--;
-			} else if (following.y === this.y) {
-				following.x > this.x ? this.x++ : this.x--;
-			} else {
-				this.x += Math.sign(following.x - this.x);
-				this.y += Math.sign(following.y - this.y);
-			}
-		}
-	};
 }
 
+Point.prototype.neighbors = function (point) {
+	return (Math.abs(this.x - point.x) <= 1 &&
+		Math.abs(this.y - point.y) <= 1);
+};
+
+Point.prototype.move = function (direction, following = null) {
+	// Moves only 1 cell
+	const dirs = {
+		U: [0, 1],
+		D: [0, -1],
+		R: [1, 0],
+		L: [-1, 0],
+	};
+	const [x, y] = dirs[direction];
+	if (following) {
+		this.moveFollowing(following);
+	} else {
+		this.x += x;
+		this.y += y;
+	}
+};
+
+Point.prototype.moveFollowing = function (following) {
+	if (!this.neighbors(following)) {
+		if (following.x === this.x) {
+			following.y > this.y ? this.y++ : this.y--;
+		} else if (following.y === this.y) {
+			following.x > this.x ? this.x++ : this.x--;
+		} else {
+			this.x += Math.sign(following.x - this.x);
+			this.y += Math.sign(following.y - this.y);
+		}
+	}
+};
+
 function Rope(size) {
+	if (size < 1) {
+		throw new Error('Rope size must be at least 1');
+	}
 	this.knots = [];
 	for (let i = 0; i < size; i++) {
 		this.knots.push(new Point(0, 0));
 	}
+}
 
-	this.move = function (direction, number, set = null) {
-		for (let i = 0; i < number; i++) {
-			for (const [idx, knot] of this.knots.entries()) {
-				if (idx === 0) {
-					knot.move(direction);
-				} else {
-					knot.move(direction, this.knots[idx - 1]);
-				}
-			}
-			if (set) {
-				const last = this.knots.at(-1);
-				set.add([last.x, last.y].toString());
+Rope.prototype.move = function (direction, number, set = null) {
+	for (let i = 0; i < number; i++) {
+		for (const [idx, knot] of this.knots.entries()) {
+			if (idx === 0) {
+				knot.move(direction);
+			} else {
+				knot.move(direction, this.knots[idx - 1]);
 			}
 		}
-	};
-}
+		if (set) {
+			const last = this.knots.at(-1);
+			set.add([last.x, last.y].toString());
+		}
+	}
+};
 
 function parseInput(textInput) {
 	const strings = textInput.trim().split('\n').map((x) => x.split(' '));
